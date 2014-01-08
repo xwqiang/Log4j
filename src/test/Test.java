fede7fd66a05970b03c69ba70b8e0b6069e9c241
xwqiang <642143714@qq.com>
2014-01-08 17:47:22 +0800

    init

---
+++ src/test/Test.java
@@ -0,0 +1,16 @@
+package test;
+import org.apache.log4j.Logger;
+
+
+public class Test {
+	static Logger logger = Logger.getLogger("test.Test");
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 logger.error("Did it again!");
+	}
+
+}
